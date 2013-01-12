48190f7fe3e4b41b39f611dca4082b4438929125
Emil Hedaya <ehedaya@gmail.com>
2013-01-11 23:15:57 -0500

    Loops and Flash source files for loops

---
+++ loops/Tuners/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+	<head>
+		<link href="../../components/bootstrap/docs/assets/css/bootstrap.css" type="text/css" rel="stylesheet" />
+		<link href="../../components/bootstrap/docs/assets/css/bootstrap-responsive.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="row">
+					<div class="span12">
+						<h1>Guitar tuners</h1>
+						<ul class="media-list">
+							<?php
+							$directory = "";
+							$loops = glob($directory . "*.swf");
+							foreach ($loops as $loop) {
+							?>
+							<li class="media">
+								<div class="media-body">
+									<object type="application/x-shockwave-flash" 
+									  data="<?php echo $loop; ?>" 
+									  width="300" height="100">
+									  <param name="movie" value="<?php echo $loop; ?>" />
+									  <param name="quality" value="high"/>
+									</object>
+								</div>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
