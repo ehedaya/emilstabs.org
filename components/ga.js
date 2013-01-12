bef4ec33f787f523bf6a8aade0df32c056b174ac
Emil Hedaya <ehedaya@gmail.com>
2013-01-12 16:39:49 -0500

    Removing unnecessary script tag

--- components/ga.js
+++ components/ga.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-5203650-3']);
   _gaq.push(['_trackPageview']);
@@ -9,5 +7,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
-</script>
